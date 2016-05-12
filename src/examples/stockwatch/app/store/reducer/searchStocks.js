0c01f5aa329587d981099d532d87dca8a652a54b
Warren Mira <warrenmira@gmail.com>
2016-05-12 22:11:27 +0800

    filter stocks on watchlist

--- src/examples/stockwatch/app/store/reducer/searchStocks.js
+++ src/examples/stockwatch/app/store/reducer/searchStocks.js
@@ -1,22 +1,27 @@
 import { UNIVERSE } from './universe';
 
 export const searchStocks = ( state, {searchText} ) => {
+
     const searchLc = searchText.toLowerCase();
+    const { watchlist } = state;
 
-    const results = { searchResults: {
+    const searchResults = {
         searchText,
-        results: UNIVERSE.filter( stock => {
-            const { ticker, company } = stock;
-            const tickerLc = ticker.toLowerCase();
-            const companyLc = company.toLowerCase();
+        results: UNIVERSE            
+            .filter( stock => watchlist.findIndex( ws => ws.ticker === stock.ticker) < 0 )
+            .filter( stock => {
+                const { ticker, company } = stock;
+                const tickerLc = ticker.toLowerCase();
+                const companyLc = company.toLowerCase();
 
-            if ( tickerLc === searchText ) {
-                return true;
-            }
-            return ticker === searchText ||
-                tickerLc.indexOf(searchLc) >= 0 ||
-                companyLc.indexOf(searchLc) >= 0;
-        })
-    }};
-    return results;
+                if ( tickerLc === searchText ) {
+                    return true;
+                }
+                return ticker === searchText ||
+                    tickerLc.indexOf(searchLc) >= 0 ||
+                    companyLc.indexOf(searchLc) >= 0;
+            })
+
+    };
+    return { searchResults };
 };
